3ff5e43b446c5c9f278acd7e4dd7690a228db7b5
Tanvi-269 <desaletanvi269@gmail.com>
2026-03-21 18:22:36 +0530

    Update server.js

--- server.js
+++ server.js
@@ -397,11 +397,13 @@ app.post("/contact", (req, res) => {
 
         // 2️⃣ Send Email
       const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS
-    }
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
 });
         try {
             await transporter.sendMail({
@@ -441,13 +443,14 @@ app.get("/test-email", async (req, res) => {
     console.log("Testing email...");
 
     const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-            user: process.env.EMAIL_USER,
-            pass: process.env.EMAIL_PASS
-        }
-    });
-
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
+  }
+});
     try {
         await transporter.sendMail({
             from: process.env.EMAIL_USER,
